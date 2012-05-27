d6d50002a1a05172d824810ef9da597f9c4e0cc8
Mike Bannister <notimpossiblemike@gmail.com>
2012-05-27 02:00:49 -0400

    cleanup

--- src/forms/package.js
+++ src/forms/package.js
@@ -8,7 +8,9 @@ Package.on_use(function (api) {
   api.use('templating', 'client');
   // Scripts
   api.add_files('vendor/underscore.strings.js', 'client');
-  api.add_files('templates.html', 'client');
+  api.add_files('templates/actions.html', 'client');
+  api.add_files('templates/base.html', 'client');
+  api.add_files('templates/inputs.html', 'client');
   api.add_files('client.js', 'client');
   api.add_files('forms.css', 'client');
 });
