e9181e575c78f4d45099e08d75997d9fac7a3bec
Mike Bannister <notimpossiblemike@gmail.com>
2012-06-02 19:55:02 +0200

    handle routes dynamically

--- demo/client/client.js
+++ demo/client/client.js
@@ -1,58 +1,12 @@
 var DemoRouter = Backbone.Router.extend({
-  // TODO figure out how to do this dynamically
-  routes: {
-    "":                          "basic",
-    "configuration/basic":       "basic",
-    "configuration/horizontal":  "horizontal",
-    "configuration/search":      "search",
-    "configuration/inline":      "inline",
-    "events":                    "events",
-    "liveEdit":                  "liveEdit",
-    "validation/clientSideOnly": "clientSideOnly",
-    "validation/manualServer":   "manualServer",
-    "validation/autoServer":     "autoServer",
-    "validation/models":         "models",
-    "validation/mongoHooks":     "mongoHooks"
+
+  initialize: function() {
+    this.route(/([^\?]*)/, 'show');
   },
-  basic: function() {
-    $('.tabbable a[href="configuration/basic"]').tab('show');
+  
+  show: function(page) {
+    $('.tabbable a[href="' + page + '"]').tab('show');
   },
-  horizontal: function() {
-    $('.tabbable a[href="configuration/horizontal"]').tab('show');
-  },
-  search: function() {
-    $('.tabbable a[href="configuration/search"]').tab('show');
-  },
-  inline: function() {
-    $('.tabbable a[href="configuration/inline"]').tab('show');
-  },
-  validation: function() {
-    $('.tabbable a[href="validation"]').tab('show');
-  },
-  events: function() {
-    $('.tabbable a[href="events"]').tab('show');
-  },
-  liveEdit: function() {
-    $('.tabbable a[href="liveEdit"]').tab('show');
-  },
-  inPlaceEditing: function() {
-    $('.tabbable a[href="inPlaceEditing"]').tab('show');
-  },
-  clientSideOnly: function() {
-    $('.tabbable a[href="validation/clientSideOnly"]').tab('show');
-  },
-  manualServer: function() {
-    $('.tabbable a[href="validation/manualServer"]').tab('show');
-  },
-  autoServer: function() {
-    $('.tabbable a[href="validation/autoServer"]').tab('show');
-  },
-  models: function() {
-    $('.tabbable a[href="validation/models"]').tab('show');
-  },
-  mongoHooks: function() {
-    $('.tabbable a[href="validation/mongoHooks"]').tab('show');
-  }
 });
 
 var demoApp = new DemoRouter();
