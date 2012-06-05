cfac27e1a1d5b4d41b4500129c46b4a9a7bb713d
Mike Bannister <notimpossiblemike@gmail.com>
2012-06-05 09:57:04 +0200

    moved router into it's own file

--- demo/client/client.js
+++ demo/client/client.js
@@ -1,41 +1,6 @@
 Users = new Meteor.Collection('users');
 Meteor.subscribe('users');
 
-var DemoRouter = Backbone.Router.extend({
-
-  // Route everything through showTag(), it'll
-  // know what to do
-  initialize: function() {
-    this.route(/([^\?]*)?/, 'showTab');
-  },
-  
-  // Figures out the correct tab based on the
-  // url and shows it
-  showTab: function(page) {
-    page || (page = this._defaultPage());
-    $('.tabbable a[href="' + page + '"]').tab('show');
-  },
-  
-  // Figure out the default tab
-  _defaultPage: function() {
-    var $default = $('.tabbable .default-tab');
-    if ($default.length > 0)
-      return $default.attr('href');
-  }
-});
-
-var demoAppRouter = new DemoRouter();
-
-Meteor.startup(function() {
-  
-  // Start router
-  Backbone.history.start({ pushState: true });
-  
-  // Add tooltips
-  $('.container').tooltip({ selector: 'a[rel=tooltip]' });
-  
-});
-
 Template.validationModelsDemo.users = function() {
   return Users.find();
 };
