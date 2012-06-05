edabf1988bc3e44a16c165172a5960d0ad42e4dd
Mike Bannister <notimpossiblemike@gmail.com>
2012-06-05 06:38:03 +0200

    moved validationFilter into validation lib

--- src/forms/server.js
+++ src/forms/server.js
@@ -4,36 +4,9 @@ Form = function(options) {
 
 Form.prototype.addValidationFilter = function(options) {
   if (options.method) {
-
-    // This is the filter we add to the targeted method
-    var validationFilter = function validationFilter(form) {
-
-      var validator;
-      var validatorClass;
-      var modelClass;
-
-      // Figure out the validator class
-      if (form._modelName) {
-        modelClass = _.constantize(form._modelName);
-        validator = new modelClass(form);
-      } else {
-        validatorClass = _.constantize(options.name + 'Validator');
-        validator = new validatorClass(form);
-      }
-
-      // Check it out!
-      if (!validator.isValid())
-        throw validator.errors;
-        
-      if (modelClass)
-        return validator;
-      else
-        return form;
-    };
-
     Filter.methods([
       {
-        handler: validationFilter,
+        handler: ValidationFilters.validationFilter,
         only: options.method
       } 
     ]);
