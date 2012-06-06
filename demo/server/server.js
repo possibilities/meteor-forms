d3e7827366fc111b0b20e089c0724303f9dae104
Mike Bannister <notimpossiblemike@gmail.com>
2012-06-06 07:07:05 +0200

    inject clientId

--- demo/server/server.js
+++ demo/server/server.js
@@ -3,26 +3,38 @@ SimpleDemo.load({
   repo: 'meteor-forms'
 });
 
+Users.remove({});
+
 Meteor.publish('users', function(sessionId) {
 
-  if (sessionId && Users.find({ _sessionId: sessionId }).count() <= 0) {
+  if (sessionId && Users.find({ _clientId: sessionId }).count() <= 0) {
 
     Users.insert({
       firstName: 'Yngwie',
       lastName: 'Malmsteen',
       about: 'Yngwie plays with yarn and is fat.',
-      _sessionId: sessionId
+      _clientId: sessionId
     });
 
     Users.insert({
       firstName: 'Derrida',
       lastName: 'Bannister',
       about: "Derrida will eat anything and shouldn't be let outside.",
-      _sessionId: sessionId
+      _clientId: sessionId
     });
   }
-
+  
   return Users.find({
-    _sessionId: sessionId
+    _clientId: sessionId
   });
 });
+
+// TODO Kinda hacky, make it less so
+Filter.methods([
+  {
+    handler: FormFilters.injectClientIdIf(function(arg) {
+      return !_.isUndefined(arg._modelName);
+    }),
+    only: 'saveModel'
+  } 
+]);
